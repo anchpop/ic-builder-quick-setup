d7bc1cf69985f75b9d8a8837efcf5216a30cdb6a
Andre Popovitch <andre@popovit.ch>
2023-02-06 23:31:26 +0000

    switch to bazel

--- download.sh
+++ download.sh
@@ -12,14 +12,6 @@ then
     # Clone the IC repo
     git -C "$base_directory" clone git@gitlab.com:dfinity-lab/public/ic.git
     git-branchless -C "$directory" init # Assumes you're using git-branchless
-
-    # setup direnv
-    echo "use nix" >> "$directory/rs/.envrc"
-    direnv allow "$directory/rs"
-
-    # setup precommit
-    (cd "$directory"; pre-commit install)
-    
 else 
     echo "Not cloning ic repo because $directory already exists"
 fi
@@ -29,4 +21,4 @@ echo ""
 echo "============"
 echo ""
 
-echo "The ic repo should be downloaded (in $directory)! Make sure you're using zsh to get direnv and other nice things."
+echo "The ic repo should be downloaded (in $directory)! Also, make sure to run `zsh`."
